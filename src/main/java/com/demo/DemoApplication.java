dda7c271f54e107f905a95c034778b119d1248e4
pavankumar0077 <dasarepavan007@gmail.com>
2022-11-12 21:40:03 +0530

    logger added

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("it's a continues integration job");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- src/test/java/com/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	
+	static Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("It's a test class");
+		assertEquals(true, true);
+		
 	}
 
 }
